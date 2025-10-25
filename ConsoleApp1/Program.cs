7f360810def3dd79ff08b4030995509b7c179658
vett5432 <groliz29@gmail.com>
2025-10-25 11:13:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x= ");
+            double x = double.Parse(Console.ReadLine());
+            if (x == 0 || x < -1)
+            {
+                Console.WriteLine("Функция не определена");
+            }
+            else
+            {
+                double y = Math.Log10(x - 1);
+                Console.Write("y= {0:f2}", y);
+            }
+        }
+    }
+}
