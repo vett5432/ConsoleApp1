62971e384e670997ba11d9944b7fc93de39e9ee4
vett5432 <groliz29@gmail.com>
2025-10-28 13:52:37 +0300

    удаление части кода

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,23 +22,4 @@ namespace ConsoleApp1
                     continue;
                 }
 
-                if (!double.TryParse(input, NumberStyles.Float | NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out x))
-                {
-                    Console.WriteLine("Некорректный формат числа. Введите число, например 2.5");
-                    continue;
-                }
 
-                if (x <= 1)
-                {
-                    Console.WriteLine("Функция не определена для x <= 1. Требуется x > 1.");
-                    continue;
-                }
-
-                break;
-            }
-
-            double y = Math.Log10(x - 1);
-            Console.WriteLine("y = {0:F2}", y);
-        }
-    }
-}
